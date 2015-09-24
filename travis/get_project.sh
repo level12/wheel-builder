662a8e1637328d59afd027ee9a42d53887455830
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-09-24 09:14:50 -0400

    pull down the submodules as well

--- travis/get_project.sh
+++ travis/get_project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git clone --depth=1 --branch $PROJECT_REF $PROJECT_URL project
+git clone --recursive --depth=1 --branch $PROJECT_REF $PROJECT_URL project
 cd project
 git checkout $PROJECT_REF
 python setup.py bdist_wheel
