0a1adb7f08df1c8e9ec036c89bfd82c21a8b2aed
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-10-13 16:10:50 -0400

    I will get this, I swear

--- travis/get_project.sh
+++ travis/get_project.sh
@@ -4,4 +4,4 @@ git clone --depth=1 --branch $PROJECT_REF $PROJECT_URL project
 cd project
 git checkout $PROJECT_REF
 mkdir dist
-pip wheel --wheel-dir=./dist $PACKAGE_NAME
+pip wheel --wheel-dir=./dist psycopg2
