1c116efb7262a356b160741bf9ea87cae6365696
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-10-13 16:06:48 -0400

    Use pip wheel instead of dist

--- travis/get_project.sh
+++ travis/get_project.sh
@@ -3,4 +3,5 @@
 git clone --depth=1 --branch $PROJECT_REF $PROJECT_URL project
 cd project
 git checkout $PROJECT_REF
-python setup.py bdist_wheel
+mkdir dist
+pip wheel --wheel-dir=./dist $PACKAGE_NAME
